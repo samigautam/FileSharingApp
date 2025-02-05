8ff6139cdd18743aa436d9bfa57a7e82357c5d77
Sami <samigautam20@gmail.com>
2025-02-05 13:14:28 +0545

    frontend done

--- fileupload/urls.py
+++ fileupload/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for fileupload project.
+"""fileupload URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -22,9 +21,14 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from home.views import *
 
 urlpatterns = [
+    path('' , home),
+    path('download/<uid>/' ,download),
     path('handle/', HandleFileUpload.as_view()),
+   
     path('admin/', admin.site.urls),
 ]
+
+
 
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,
--- home/views.py
+++ home/views.py
@@ -1,31 +1,40 @@
+from tkinter import E
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .serializers import *
-from .models import File
+from rest_framework.parsers import MultiPartParser
+
+
+def home(request):
+    return render(request ,'home.html')
+
+
+
+def download(request , uid):
+    return render(request , 'download.html' , context = {'uid' : uid})
 
 class HandleFileUpload(APIView):
-    def post(self, request):
+    parser_classes = [MultiPartParser]
+    def post(self , request):
         try:
             data = request.data
 
-            serializer = FileListSerializer(data=data)
+            serializer = FileListSerializer(data = data)
+        
             if serializer.is_valid():
                 serializer.save()
                 return Response({
-                    'status': 200,
-                    'message' : 'files uploaded successfully'
+                    'status' : 200,
+                    'message' : 'files uploaded successfully',
+                    'data' : serializer.data
                 })
+            
             return Response({
-                'status': 400,
-                'message': 'Something Went wrong',
-                'data': serializer.errors
+                'status' : 400,
+                'message' : 'somethign went wrong',
+                'data'  : serializer.errors
             })
-
         except Exception as e:
-            return Response({  # Added this return statement
-                'status': 500,
-                'message': 'Internal Server Error',
-                'error': str(e)  # Include error message for debugging
-            }, status=500)
+            print(e)
